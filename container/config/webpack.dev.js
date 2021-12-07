d414d8124a65ed59a9099a6e39ac7a238d29db11
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-12-07 11:52:32 +0000

    Sharing dependencies

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -16,6 +17,7 @@ const devConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: { marketing: "marketing@http://localhost:8081/remoteEntry.js" },
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({ template: "./public/index.html" }),
   ],
--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -17,6 +18,7 @@ const devConfig = {
       name: "marketing",
       filename: "remoteEntry.js",
       exposes: { "./MarketingApp": "./src/bootstrap" },
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({ template: "./public/index.html" }),
   ],
