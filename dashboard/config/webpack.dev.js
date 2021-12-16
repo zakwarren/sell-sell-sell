55c1d834a3255c50114617e4bf35d0fed467fc5d
zakwarren <41547855+zakwarren@users.noreply.github.com>
2021-12-16 16:53:13 +0000

    Setup dashboard project

---
+++ dashboard/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const port = 8083;
+
+const devConfig = {
+  mode: "development",
+  output: { publicPath: `http://localhost:${port}/` },
+  devtool: "eval-source-map",
+  devServer: {
+    port: port,
+    historyApiFallback: {
+      index: "/index.html",
+    },
+    headers: { "Access-Control-Allow-Origin": "*" },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: { "./DashboardApp": "./src/bootstrap" },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
